d006c6fb39b9c59dd1e626f036cd21a663247d8f
mrfirefox137 <mrfirefox137@protonmail.com>
2025-11-25 20:50:34 +0800

    completed CH2, started CH1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import num_words
+from stats import count_char
 
 def get_book_text(filepath):
 	with open(filepath) as f:
@@ -9,6 +10,7 @@ def main():
 	contents = get_book_text("books/frankenstein.txt")
 	numWords = num_words(contents)
 	print(f"Found {numWords} total words")
-
+	char_count = count_char(contents)
+	print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -4,5 +4,16 @@ def num_words(contents):
     return numWords
 
 def count_char(contents):
+	unique = set(contents.lower())
+	#print(unique)
+	unique_dict = {}
+	for char in unique:
+		count = contents.count(char) + contents.count(char.upper())
+		unique_dict[char] = count
+	return unique_dict
+
+def sort_alpha(char_count):
+	 
 	
+
 	  
